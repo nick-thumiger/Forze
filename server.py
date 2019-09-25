24b9120adb25c846a02c49bf7eb68d9017eed274
nick-thumiger <nicholasthumiger@gmail.com>
2019-09-25 14:03:28 +0000

    serverside 2

--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from init import bootstrap_system
-from routes import *
-
-app = Flask(__name__)
-app.secret_key = 'very-secret-123'  # Used to add entropy
-
-
-system = bootstrap_system()
