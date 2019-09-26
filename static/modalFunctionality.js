6bfc7369e2502529219424f2b050ad00bffe3c34
Jaiki Pitt <jaiki.pitt@student.tsc.nsw.edu.au>
2019-09-26 13:45:35 +1000

    changed databases

--- static/modalFunctionality.js
+++ static/modalFunctionality.js
@@ -1,5 +1,5 @@
-let apiURL = 'http://127.0.0.1:5100';
-// let apiURL = 'http://forze.pythonanywhere.com';
+// let apiURL = 'http://127.0.0.1:5100';
+let apiURL = 'http://forze.pythonanywhere.com';
 
 let currentEditCategory = null;
 let currentEditID = null;
